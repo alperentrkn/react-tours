d9d054143827dc107d2e1c00255a6410baa03bb2
alperentrkn <48105284+alperentrkn@users.noreply.github.com>
2019-06-10 02:16:44 +0300

    Add files via upload

---
+++ tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info:
+          "Turizmin doğası, cazibesi, son yarım asırdaki dramatik oranda büyümesi, gelişmiş ve gelişmemiş toplumlara katkısı ve en önemlisi yavaş yavaş kurumsallaşması en değerli özelliklerinden biri. Turizmin doğuş anından itibaren emin adımlarla ticarileştirilmesi ve organize olması da en büyük temellerinden biri."
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info:
+          "Turizmin doğası, cazibesi, son yarım asırdaki dramatik oranda büyümesi, gelişmiş ve gelişmemiş toplumlara katkısı ve en önemlisi yavaş yavaş kurumsallaşması en değerli özelliklerinden biri. Turizmin doğuş anından itibaren emin adımlarla ticarileştirilmesi ve organize olması da en büyük temellerinden biri."
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london royal palace tour",
+    info:
+          "Turizmin doğası, cazibesi, son yarım asırdaki dramatik oranda büyümesi, gelişmiş ve gelişmemiş toplumlara katkısı ve en önemlisi yavaş yavaş kurumsallaşması en değerli özelliklerinden biri. Turizmin doğuş anından itibaren emin adımlarla ticarileştirilmesi ve organize olması da en büyük temellerinden biri."
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+          "Turizmin doğası, cazibesi, son yarım asırdaki dramatik oranda büyümesi, gelişmiş ve gelişmemiş toplumlara katkısı ve en önemlisi yavaş yavaş kurumsallaşması en değerli özelliklerinden biri. Turizmin doğuş anından itibaren emin adımlarla ticarileştirilmesi ve organize olması da en büyük temellerinden biri."  
+  }
+];
